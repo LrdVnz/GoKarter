c67d5d8a1895aca7ccf5a8a35f8d7e67073a3544
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-06-12 22:11:10 +0200

    added footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { MDBFooter, MDBIcon } from 'mdb-react-ui-kit';
 import "./styles.css"
 
 const Footer = (props) => {
   return (
-    <footer
-    >
-      <Container>
-        <span>
-          {`${new Date().getFullYear()} GoKarter - A leaderboard for go kart races`}
-        </span>
-        <span>
-          <p><a href="https://github.com/LrdVnz">My github</a></p>
-        </span>
-      </Container>
-    </footer>
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+    <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+      <div className='me-5 d-none d-lg-block'>
+        <span>Get connected with us on social networks:</span>
+      </div>
+
+      <div>
+        <a href='' className='me-4 text-reset'>
+          <MDBIcon fab icon="google" />
+        </a>
+        <a href='' className='me-4 text-reset'>
+          <MDBIcon fab icon="linkedin" />
+        </a>
+        <a href='' className='me-4 text-reset'>
+          <MDBIcon fab icon="github" />
+        </a>
+      </div>
+    </section>
+    </MDBFooter>
   );
 };
 
